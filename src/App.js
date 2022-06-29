3e68580fbac3d87ac710d8fb19da01d766620066
kypuwinston <vitalik021190@outlook.com>
2022-06-29 20:41:07 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import './App.css';
+import ProductTable from './components/MyTable';
+
+function App() {
+
+  const data = [
+  {type: "pdf", name: "Чек операции", signStat: "На подписании", attStat: "Готов", ref: <a href="http://localhost:3000">Документ</a>},
+  {type: "pdf", name: "Чек операции", signStat: "На подписании", attStat: "В ожидании", ref: <a href="http://localhost:3000">Документ</a>},
+  {type: "pdf", name: "Чек операции", signStat: "В ожидании", attStat: "В ожидании", ref: <a href="http://localhost:3000">Документ</a>},
+  {type: "pdf", name: "Чек операции", signStat: "На подписании", attStat: "В ожидании", ref: <a href="http://localhost:3000">Документ</a>},
+  {type: "pdf", name: "Чек операции", signStat: "На подписании", attStat: "В ожидании", ref: <a href="http://localhost:3000">Документ</a>},
+  {type: "pdf", name: "Чек операции", signStat: "В ожидании", attStat: "Готов", ref: <a href="http://localhost:3000">Документ</a>},
+  {type: "pdf", name: "Чек операции", signStat: "На подписании", attStat: "В ожидании", ref: <a href="http://localhost:3000">Документ</a>},
+  {type: "pdf", name: "Чек операции", signStat: "На подписании", attStat: "В ожидании", ref: <a href="http://localhost:3000">Документ</a>},
+  {type: "pdf", name: "Чек операции", signStat: "В ожидании", attStat: "Отказан", ref: <a href="http://localhost:3000">Документ</a>},
+  {type: "pdf", name: "Чек операции", signStat: "На подписании", attStat: "В ожидании", ref: <a href="http://localhost:3000">Документ</a>},
+  {type: "pdf", name: "Чек операции", signStat: "На подписании", attStat: "В ожидании", ref: <a href="http://localhost:3000">Документ</a>},
+  {type: "pdf", name: "Чек операции", signStat: "В ожидании", attStat: "В ожидании", ref: <a href="http://localhost:3000">Документ</a>},
+  {type: "pdf", name: "Чек операции", signStat: "В ожидании", attStat: "Готов", ref: <a href="http://localhost:3000">Документ</a>},
+  {type: "pdf", name: "Чек операции", signStat: "На подписании", attStat: "Отказан", ref: <a href="http://localhost:3000">Документ</a>},
+  {type: "pdf", name: "Чек операции", signStat: "На подписании", attStat: "Готов", ref: <a href="http://localhost:3000">Документ</a>},
+  {type: "doc", name: "Какой-то документ", signStat: "На подписании", attStat: "В ожидании", ref: <a href="http://localhost:3000">Документ</a>},
+  {type: "doc", name: "Какой-то документ", signStat: "В ожидании", attStat: "Готов", ref: <a href="http://localhost:3000">Документ</a>},
+  {type: "doc", name: "Какой-то документ", signStat: "На подписании", attStat: "В ожидании", ref: <a href="http://localhost:3000">Документ</a>},
+  {type: "doc", name: "Какой-то документ", signStat: "На подписании", attStat: "В ожидании", ref: <a href="http://localhost:3000">Документ</a>},
+  {type: "doc", name: "Какой-то документ", signStat: "В ожидании", attStat: "Отказан", ref: <a href="http://localhost:3000">Документ</a>},
+  {type: "doc", name: "Какой-то документ", signStat: "На подписании", attStat: "В ожидании", ref: <a href="http://localhost:3000">Документ</a>},
+  {type: "doc", name: "Какой-то документ", signStat: "На подписании", attStat: "В ожидании", ref: <a href="http://localhost:3000">Документ</a>},
+  {type: "doc", name: "Какой-то документ", signStat: "В ожидании", attStat: "В ожидании", ref: <a href="http://localhost:3000">Документ</a>},
+  {type: "doc", name: "Какой-то документ", signStat: "В ожидании", attStat: "Готов", ref: <a href="http://localhost:3000">Документ</a>},
+  {type: "doc", name: "Какой-то документ", signStat: "На подписании", attStat: "Отказан", ref: <a href="http://localhost:3000">Документ</a>},
+  {type: "doc", name: "Какой-то документ", signStat: "На подписании", attStat: "Готов", ref: <a href="http://localhost:3000">Документ</a>},
+  {type: "img", name: "Фото объекта", signStat: "На подписании", attStat: "В ожидании", ref: <a href="http://localhost:3000">Документ</a>},
+  {type: "img", name: "Фото объекта", signStat: "В ожидании", attStat: "Готов", ref: <a href="http://localhost:3000">Документ</a>},
+  {type: "img", name: "Фото объекта", signStat: "На подписании", attStat: "В ожидании", ref: <a href="http://localhost:3000">Документ</a>},
+  {type: "img", name: "Фото объекта", signStat: "На подписании", attStat: "В ожидании", ref: <a href="http://localhost:3000">Документ</a>},
+  {type: "img", name: "Фото объекта", signStat: "В ожидании", attStat: "Отказан", ref: <a href="http://localhost:3000">Документ</a>},
+  {type: "img", name: "Фото объекта", signStat: "На подписании", attStat: "В ожидании", ref: <a href="http://localhost:3000">Документ</a>},
+  {type: "img", name: "Фото объекта", signStat: "На подписании", attStat: "В ожидании", ref: <a href="http://localhost:3000">Документ</a>},
+  {type: "img", name: "Фото объекта", signStat: "В ожидании", attStat: "В ожидании", ref: <a href="http://localhost:3000">Документ</a>},
+  {type: "img", name: "Фото объекта", signStat: "В ожидании", attStat: "Готов", ref: <a href="http://localhost:3000">Документ</a>},
+  {type: "img", name: "Фото объекта", signStat: "На подписании", attStat: "Отказан", ref: <a href="http://localhost:3000">Документ</a>}
+  ]
+
+  return (
+    <div className="App">
+      <ProductTable docs={data}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MyTable.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import './MyTable.css';
+
+function ProductTable(props) {
+    const { docs } = props;
+    const [sortConfig, setSortConfig] = React.useState(null);
+
+    React.useMemo(() => {
+      if (sortConfig != null) {
+        docs.sort((a, b) => {
+          if (a[sortConfig.key] < b[sortConfig.key]) {
+            return sortConfig.direction === 'ascending' ? -1 : 1;
+          }
+          if (a[sortConfig.key] > b[sortConfig.key]) {
+            return sortConfig.direction === 'ascending' ? 1 : -1;
+          }
+          return 0;
+        });
+      }
+    }, [docs, sortConfig]);
+
+    const requestSort = key => {
+      let direction = 'ascending';
+      if (sortConfig != null) {
+        if (sortConfig.key === key && sortConfig.direction === 'ascending') {
+          direction = 'descending';
+        }
+      }
+      setSortConfig({ key, direction});
+    }
+
+    return (
+      <div className="container">
+      <table className="table">
+        <thead>
+          <tr>
+            <th>
+              <button type="button" onClick={() => requestSort('type')}>
+              Тип документа
+              </button>
+            </th>
+            <th>
+              <button type="button" onClick={() => requestSort('name')}>
+              Название документа
+              </button>
+            </th>
+            <th>
+              <button type="button" onClick={() => requestSort('signStat')}>
+              Статус подписания документа
+              </button>
+            </th>
+            <th>
+              <button type="button" onClick={() => requestSort('attStat')}>
+              Статус прикрепления документа
+              </button>
+            </th>
+            <th>
+              <button type="button">
+              Ссылка на документ
+              </button>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {docs.map((doc, i) => (
+            <tr key={i}>
+              <td>{doc.type}</td>
+              <td>{doc.name}</td>
+              <td>{doc.signStat}</td>
+              <td>{doc.attStat}</td>
+              <td align="center">{doc.ref}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      </div>
+    );
+  }
+
+  export default ProductTable;
